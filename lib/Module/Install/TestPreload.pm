71a8164ddf0e35363f2b56d75e2dc51ea3f3e98b
xaicron <xaicron@gmail.com>
2010-06-28 01:28:00 +0900

    initial commit

---
+++ lib/Module/Install/TestPreload.pm
@@ -0,0 +1,32 @@
+package Module::Install::TestPreload;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Module::Install::TestPreload -
+
+=head1 SYNOPSIS
+
+  use Module::Install::TestPreload;
+
+=head1 DESCRIPTION
+
+Module::Install::TestPreload is
+
+=head1 AUTHOR
+
+Yuji Shimada E<lt>xaicron {at} cpan.orgE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
