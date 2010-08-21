d0f20c0b563c360f90f34336e1ed89d0777a7d50
xaicron <xaicron@gmail.com>
2010-08-22 00:34:31 +0900

    write pod

--- lib/Module/Install/TestPreload.pm
+++ lib/Module/Install/TestPreload.pm
@@ -67,29 +67,92 @@ __END__
 
 =head1 NAME
 
-Module::Install::TestPreload - 
+Module::Install::TestPreload - preload codes for test
 
 =head1 SYNOPSIS
 
   # in Makefile.PL
   use inc::Module::Install;
   tests 't/*t';
+  test_preload_inc    "$ENV{HOME}/perl5/lib";
   test_preload_module 'Foo';
   test_preload_script 'bar.pl';
   test_preload_codes  'print scalar localtime, "\n"';
   
   # maybe make test is
-  perl -MExtUtils::Command::MM -MFoo -e "do 'bar.pl'; sub { print scalar localtime, \"\n\" }->(); test_harness(0, 'inc')" t/*t
+  perl -MExtUtils::Command::MM -I/home/xaicron/perl5/lib -MFoo -e "do 'bar.pl'; sub { print scalar localtime, \"\n\" }->(); test_harness(0, 'inc')" t/*t
 
 =head1 DESCRIPTION
 
-Module::Install::TestPreload is
+Module::Install::TestPreload is 
+
+=head1 FUNCTIONS
+
+=over
+
+=item test_preload_module(@modules)
+
+Setting preload modules in make test command.
+
+  use inc::Module::Install;
+  tests 't/*t';
+  test_preload_module('Foo', 'Bar::Baz');
+  
+  # maybe make test is
+  perl -MExtUtils::Command::MM -MFoo -MBar::Baz -e "test_harness(0, 'inc')" t/*t
+
+=item test_preload_inc(@inc_paths)
+
+Setting preload module include paths in make test command.
+
+  use inc::Module::Install;
+  tests 't/*t';
+  test_preload_inc('/path/to/lib');
+  
+  # maybe make test is
+  perl -MExtUtils::Command::MM -I/path/to/lib -e "test_harness(0, 'inc')" t/*t
+
+=item test_preload_script(@script_files)
+
+Setting preload script files in make test command.
+
+  use inc::Module::Install;
+  tests 't/*t';
+  test_preload_script('/path/to/script.pl');
+  
+  # maybe make test is
+  perl -MExtUtils::Command::MM "do '/path/to/script.pl'; test_harness(0, 'inc')" t/*t
+
+=item test_preload_codes(@codes)
+
+Setting preload perl codes in make test command.
+
+  use inc::Module::Install;
+  tests 't/*t';
+  test_preload_code('print scalar localtime , "\n"', sub { system qw/cat README/ });
+  
+  # maybe make test is
+  perl -MExtUtils::Command::MM "sub { print scalar localtme, "\n" }->(); sub { system qw(cat README) }->(); test_harness(0, 'inc')" t/*t
+
+The perl codes runs test_preload_script files runs later.
+
+  use inc::Module::Install;
+  tests 't/*t';
+  test_preload_script('/path/to/script.pl');
+  test_preload_code('print scalar localtime , "\n"');
+  
+  # maybe make test is
+  perl -MExtUtils::Command::MM "do '/path/to/script.pl'; sub { print scalar localtme, "\n" }->(); test_harness(0, 'inc')" t/*t
+
+=back
 
 =head1 AUTHOR
 
 Yuji Shimada E<lt>xaicron {at} cpan.orgE<gt>
 
 =head1 SEE ALSO
+
+L<Module::Install>
 
 =head1 LICENSE
 
